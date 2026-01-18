1d025456cda0d3f6b2b094f0d52e7e7d0854cc4a
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-01-18 21:20:59 +0700

    jest configuration

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,16 @@
 import { sum } from "../src/sum";
 
-test("Tes sum function", () => {
+test("Tes sum function 1", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
+test("Tes sum function 2", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
+test("Tes sum function 3", () => {
   const result = sum(1, 2);
 
   expect(result).toBe(3);
