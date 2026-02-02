230ddaf2c07b022b17747d87d2d5df475b82779e
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-02-02 10:37:05 +0700

    tags

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -15,3 +15,19 @@ test("Menggunakan Muustache cache", () => {
 
   expect(data).toBe("Halo, nama saya Muhamad Nur Rizki");
 });
+
+test("Tags", () => {
+  Mustache.parse("Halo, nama saya {{fullname}}, saya tinggal di {{{address}}}");
+
+  const data = Mustache.render(
+    "Halo, nama saya {{fullname}}, saya tinggal di {{{address}}}",
+    {
+      fullname: "Muhamad Nur Rizki",
+      address: "<b>Tangerang</b>",
+    },
+  );
+
+  expect(data).toBe(
+    "Halo, nama saya Muhamad Nur Rizki, saya tinggal di <b>Tangerang</b>",
+  );
+});
